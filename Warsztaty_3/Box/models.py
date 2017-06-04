a7db0664b96233e93003b3c622c6805987001436
Daniel Tomaszuk <dtomaszuk01@gmail.com>
2017-06-04 17:40:23 +0200

    Address and telephone adding

--- Warsztaty_3/Box/models.py
+++ Warsztaty_3/Box/models.py
@@ -2,31 +2,29 @@ from django.db import models
 
 
 class Address(models.Model):
-
     city = models.CharField(max_length=64)
     street = models.CharField(max_length=64)
     house_number = models.IntegerField()
     door_number = models.IntegerField()
-    
-    
+
+
 class Telephone(models.Model):
     number = models.IntegerField()
     types = ((1, "Home Number"), (2, "Work Number"))
     type = models.CharField(choices=types, default=1, max_length=32)
-    
-    
+
+
 class Email(models.Model):
     email = models.CharField(max_length=32)
     types = ((1, "Home Number"), (2, "Work Number"))
     type = models.CharField(choices=types, default=1, max_length=32)
-    
-    
+
+
 class Person(models.Model):
     first_name = models.CharField(max_length=32)
     last_name = models.CharField(max_length=32)
-    address = models.ForeignKey(Address) 
-    telephone = models.ForeignKey(Telephone)
-    e_mail = models.ForeignKey(Email)   
-    description = models.TextField()    
-    
 
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    telephone = models.ForeignKey(Telephone, on_delete=models.CASCADE)
+    e_mail = models.ForeignKey(Email, on_delete=models.CASCADE)
+    description = models.TextField()
--- Warsztaty_3/Box/views.py
+++ Warsztaty_3/Box/views.py
@@ -3,37 +3,36 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.db import models
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
+from django.core.exceptions import ObjectDoesNotExist
 from Box.models import *
 
 
 class ShowAll(View):
-     
-    def get(self, request):     
+    def get(self, request):
         response = HttpResponse()
         response.write("Show All")
-        return response 
+        return response
 
 
 class NewPerson(View):
     HTML = """
                 <html>
-                    <h2>You are adding new person! </h2>
+                    <h2>You are adding new person! </h2>                   
                     <form method="post" action="" id="new_form">
                         <label>First Name</label><br>
                         <input name="first_name" type="text" maxlength="255" value=""/><br>
                         <label>Last Name</label><br>
                         <input name="last_name" type="text" maxlength="255" value=""/><br>
-                        
+                        <br>
                         <label>Address:</label></br> 
                         {}
-                                
-                                                            
+                        <br>                                
                         <label>Telephone:</label><br>
-                        <a href=/add_telephone>Add Telephone..</a><br>
-                                                
+                        {}
+                        <br>                        
                         <label>E-mail:</label><br>
                         <a href=/add_email>Add Email..</a><br>
-                        
+                        <br>
                     </form>                       
                         
                     <h5>Write description of person here:</h5>                    
@@ -41,89 +40,88 @@ class NewPerson(View):
 Enter text here...
                     </textarea><br>
                     <button type="submit" name="submit" value="name" form="new_form">Add New!</button>   
-                    
-                    
+                  
                 </html>"""
 
+    def get(self, request):
+        address = None
+        in_form_address = """
+                City: {}, Street: {}, House Number: {}, Door Number: {} 
+                <a href=/add_address>Change Address..</a><br>            
+                """
 
-    def get(self, request, address=None, telephone=None, email=None):
+        telephone = None
+        in_form_telephone = """
+                Number: {}, Number Type: {} 
+                <a href=/add_telephone>Change Telephone..</a><br>            
+                """
 
         response = HttpResponse()
-        
-        address = response.write(request.GET.get('address'))
-        address = request.GET.get('address')
-
-#         response.write(self.HTML)
+        # try:  # try get data form DB
+        address = Address.objects.get(pk=1)
+        telephone = Telephone.objects.get(pk=1)
+        # except ObjectDoesNotExist:  # if there is no such tables in DB
 
         # first display with no data inputed in the address,telephone and mail
-        if address == None:
-            
-            response.write(self.HTML.format("<a href=/add_address>Add Adress..</a><br>"))    
+        if address is None:
+            response.write(self.HTML.format("<a href=/add_address>Add Address..</a><br>", in_form_telephone))
         else:
-#               response.write(address)
-            city = address[0]
-            street = address[1]
-            d_number = address[2]
-            h_number = address[3]
-            
-            in_form = """
-                    City: {}, Street: {}, House Number: {}, Door Number: {} <br>             
-                    """.format(city, street, h_number, d_number)            
-            
-            response.write(self.HTML.format(in_form)) 
-        
-        
-        
+            city = address.city
+            street = address.street
+            h_number = address.house_number
+            d_number = address.door_number
+            in_form_address = in_form_address.format(city, street, h_number, d_number)
+
+        ##################################################################
+
+        if telephone is None:
+            response.write(self.HTML.format(in_form_address, "<a href=/add_telephone>Add Telephone..</a><br>"))
+        else:
+            telephone = Telephone.objects.get(pk=1)
+            number = telephone.number
+            num_type = telephone.get_type_display()
+
+            in_form_telephone = in_form_telephone.format(number, num_type)
+
+
+        response.write(self.HTML.format(in_form_address, in_form_telephone))
         return response
-    
-    def post(self, request):        
-        
+
+    def post(self, request):
+
+        # when you want to save all person data into DB
         response = HttpResponse()
         first_name = request.POST.get('first_name')
         last_name = request.POST.get('last_name')
-                
-                
-#         Person.objects.create()
-
-
-
-
 
         response.write(first_name)
         response.write(" ")
         response.write(last_name)
-        
         return response
-     
-     
-     
-     
+
+
 class ShowPerson(View):
- 
-    def get(self, request):     
+    def get(self, request):
         response = HttpResponse()
         response.write("Show Person")
-        return response      
-     
-     
-     
-class DeletePerson(View):
+        return response
 
-    def get(self, request):     
+
+class DeletePerson(View):
+    def get(self, request):
         response = HttpResponse()
         response.write("Delete Person")
-        return response 
-     
-               
+        return response
+
+
 class ModifyPerson(View):
- 
-    def get(self, request):     
+    def get(self, request):
         response = HttpResponse()
         response.write("Modify Person")
-        return response 
-     
-     
-class AddAddress(View):    
+        return response
+
+
+class AddAddress(View):
     HTML = """
             <h2>You are adding new address! </h2>
                 <form method="post" action="" id="new_form">
@@ -141,83 +139,53 @@ class AddAddress(View):
 
                     <button type="submit" name="submit" value="name" form="new_form">Add Address!</button>   
                 </form>
-             """ 
-                   
- 
-    def get(self, request):     
-        response = HttpResponse()        
+             """
+
+    def get(self, request):
+        response = HttpResponse()
         response.write(self.HTML)
         return response
-    
-   
+
     def post(self, request):
-        response = HttpResponse()
-        
+        # response = HttpResponse()
+
         city = request.POST.get("city")
-        street = request.POST.get("street")    
+        street = request.POST.get("street")
         h_number = request.POST.get("h_number")
         d_number = request.POST.get("d_number")
-        
-#         address = Address.objects.create(city=city, street=street, house_number=h_number, door_number=d_number)
-         
-        address = ((city), (street), (d_number), (h_number))                
-#                 
-#         return HttpResponseRedirect('/new_person?city={}&street={}&h_number={}&d_number={}'.format(city, street, h_number, d_number))
-        
-        return HttpResponseRedirect('/new_person?address={}'.format(address))
+        Address.objects.create(city=city, street=street, house_number=h_number, door_number=d_number)
+        return HttpResponseRedirect('/new_person')
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
 class AddTelephone(View):
+    HTML = """
+               <h2>You are adding new telephone! </h2>
+                   <form method="post" action="" id="new_form">
+                       <label>Number:</label><br>
+                       <input name="number" type="number" value=""/><br>
+                       <label>Type of telephone:</label><br>
+                       <select name = "num_type">
+                           <option value="1">Home Number</option>
+                           <option value="2">Work Number</option>
+                       </select>
+                       <button type="submit" name="submit" value="telephone" form="new_form">Add Number!</button>   
+                   </form>
+                """
 
-    def get(self, request):     
+    def get(self, request):
         response = HttpResponse()
-        response.write("Add New Telephone")
+        response.write(self.HTML)
         return response
-        
-        
-         
+
+    def post(self, request):
+        number = request.POST.get("number")
+        num_type = request.POST.get("num_type")
+        Telephone.objects.create(number=number, type=num_type)
+        return HttpResponseRedirect('/new_person')
+
+
 class AddEmail(View):
-  
-    def get(self, request):     
+    def get(self, request):
         response = HttpResponse()
         response.write("Add New Email")
-        response.write("girhjfeigbfjewbhfjioewhbjiofewghb")
-        return response   
-        
-         
-         
-         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-    
-    
-    
-    
-    
-    
+        return response
